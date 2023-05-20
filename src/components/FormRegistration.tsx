fb31d627962d78f4cc0f975086859fbd4eaf5e48
Fernando A. Gonzalez <fagonzalez.nt@gmail.com>
2023-05-20 12:53:03 -0300

    improve style into FormRegistration component

--- src/components/FormRegistration.tsx
+++ src/components/FormRegistration.tsx
@@ -4,7 +4,7 @@ import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
-import { Box, Container } from "@mui/material";
+import { Box, Card, Container } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const FormRegistration = () => {
@@ -18,86 +18,89 @@ const FormRegistration = () => {
         spacing={2}
         sx={{ minHeight: "100vh" }}
       >
-        <Grid item>
-          <Typography variant="h5" align="center">
-            Regístrate
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Box
-            component="form"
-            sx={{
-              width: 300,
-              "& .MuiTextField-root": { marginBottom: "16px" },
-            }}
-          >
-            <TextField
-              fullWidth
-              id="nombre"
-              label="Nombre"
-              name="nombre"
-              autoComplete="nombre"
-              autoFocus
-            />
-            <TextField
-              fullWidth
-              name="apellido"
-              label="Apellido"
-              id="apellido"
-              autoComplete="apellido"
-            />
-            <TextField
-              fullWidth
-              id="email1"
-              label="Correo electrónico"
-              name="correo electronico"
-              autoComplete="email"
-            />
-            <TextField
-              fullWidth
-              id="email2"
-              label="Repetir correo electrónico"
-              name="repetir correo electronico"
-              autoComplete="email"
-            />
-            <TextField
-              fullWidth
-              name="contraseña"
-              label="Contraseña"
-              type="password"
-              id="contraseña"
-              autoComplete="current-password"
-            />
-            <TextField
-              fullWidth
-              name="repetir contraseña"
-              label="Repetir contraseña"
-              type="password"
-              id="password2"
-              autoComplete="current-password"
-            />
-
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Acuerdo de privacidad"
-            />
-          </Box>
-        </Grid>
-        <Grid item>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            <Link
-              to="/FormLogin"
-              style={{ color: "white", textDecoration: "none" }}
-            >
+        <Card sx={{ p: 5, boxShadow: 3 }}>
+          <Grid item>
+            <Typography variant="h5" align="center" sx={{ mb: 3 }}>
               Regístrate
-            </Link>
-          </Button>
-        </Grid>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Box
+              component="form"
+              sx={{
+                width: 300,
+                m: 2,
+              }}
+            >
+              <TextField
+                fullWidth
+                id="nombre"
+                label="Nombre"
+                name="nombre"
+                autoComplete="nombre"
+                autoFocus
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                name="apellido"
+                label="Apellido"
+                id="apellido"
+                autoComplete="apellido"
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                id="email1"
+                label="Correo electrónico"
+                name="correo electronico"
+                autoComplete="email"
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                id="email2"
+                label="Repetir correo electrónico"
+                name="repetir correo electronico"
+                autoComplete="email"
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                name="contraseña"
+                label="Contraseña"
+                type="password"
+                id="contraseña"
+                autoComplete="current-password"
+                sx={{ mb: 2 }}
+              />
+              <TextField
+                fullWidth
+                name="repetir contraseña"
+                label="Repetir contraseña"
+                type="password"
+                id="password2"
+                autoComplete="current-password"
+                sx={{ mb: 2 }}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Acuerdo de privacidad"
+                sx={{ mb: 2 }}
+              />
+            </Box>
+          </Grid>
+          <Grid item>
+            <Button type="submit" fullWidth variant="contained">
+              <Link
+                to="/FormLogin"
+                style={{ color: "white", textDecoration: "none" }}
+              >
+                Regístrate
+              </Link>
+            </Button>
+          </Grid>
+        </Card>
       </Grid>
     </Container>
   );
