091a9f156c8d678b54d8989b4afa387f60653f68
leandro oviedo <leanoviedo@gmail.com>
2023-10-25 01:50:53 -0300

    resolved problem in custom nav bar and mejorando ui in componets LostAndFoundList

--- src/components/LostAndFoundList.tsx
+++ src/components/LostAndFoundList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Typography,
   ListItem,
@@ -7,6 +7,8 @@ import {
   CardMedia,
   Grid,
   Paper,
+  Tabs,
+  Tab,
 } from "@mui/material";
 import { useAppSelector } from "../redux/hooks";
 import { selectLostObjects } from "../redux/slices/lostObjectSlice";
@@ -19,26 +21,47 @@ const LostAndFoundList = () => {
   const loggedInUser = useAppSelector(selectUserLogin);
 
   const userReports = lostObjects.filter(
-    (item) =>
-      (item.userReport?.email === loggedInUser?.email) ||
-      (item.userReclamed?.email === loggedInUser?.email) || (item.id === loggedInUser?.email)
+    (item) => item.userReport?.email === loggedInUser?.email
   );
+
+  const userClaims = lostObjects.filter(
+    (item) => item.userReclamed?.email === loggedInUser?.email
+  );
+
+  const [tabValue, setTabValue] = useState(0);
+
+  const handleChangeTab = (_event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
 
   return (
     <Grid container spacing={3}>
-      <CustomNavbar></CustomNavbar>
+      <CustomNavbar />
       <Grid item xs={12}>
         <Typography variant="h5" component="h1" gutterBottom align="center">
           Lista de reportes
         </Typography>
       </Grid>
       <Grid item xs={12}>
-        {userReports.length === 0 ? (
-          <Grid container alignItems="center" justifyContent="center">
-            <Typography variant="h6">No hay datos disponibles.</Typography>
-          </Grid>
-        ) : (
-          <Paper elevation={2}>
+        <Paper elevation={2}>
+          <Tabs
+            value={tabValue}
+            onChange={handleChangeTab}
+            centered
+            TabIndicatorProps={{
+              style: { background: tabValue === 0 ? "green" : "red" },
+            }}
+          >
+            <Tab
+              label="Objetos Reportados"
+              style={{ color: tabValue === 0 ? "green" : "black" }}
+            />
+            <Tab
+              label="Objetos Reclamados"
+              style={{ color: tabValue === 1 ? "red" : "black" }}
+            />
+          </Tabs>
+          {tabValue === 0 ? (
             <Grid container spacing={2}>
               {userReports.map((item, index) => (
                 <Grid item xs={6} key={index}>
@@ -66,7 +89,7 @@ const LostAndFoundList = () => {
                               <strong>Lugar:</strong> {item.city.name} {item.country.name}
                             </Typography>
                             <Typography variant="body2" color="textPrimary">
-                              <strong>Nombre: </strong> {item.userReport?.name.first}, {item.userReport?.name.last}
+                              <strong>Nombre:</strong> {item.userReport?.name.first}, {item.userReport?.name.last}
                             </Typography>
                           </React.Fragment>
                         }
@@ -76,8 +99,47 @@ const LostAndFoundList = () => {
                 </Grid>
               ))}
             </Grid>
-          </Paper>
-        )}
+          ) : (
+            <Grid container spacing={2}>
+              {userClaims.map((item, index) => (
+                <Grid item xs={6} key={index}>
+                  <Link to={`/FoundObjects/`} style={{ textDecoration: "none" }}>
+                    <ListItem alignItems="flex-start">
+                      <ListItemIcon>
+                        <CardMedia
+                          component="img"
+                          alt="Objeto perdido"
+                          height="100"
+                          image={item.photo}
+                          sx={{ padding: 2 }}
+                        />
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={item.status === "encontrado" ? "Encontrado" : "Perdido"}
+                        secondary={
+                          <React.Fragment>
+                            <Typography variant="body2" color="textPrimary">
+                              <strong>Descripción:</strong> {item.description}
+                            </Typography>
+                            <Typography variant="body2" color="textPrimary">
+                              <strong>Aeropuerto:</strong> {item.airport.name}
+                            </Typography>
+                            <Typography variant="body2" color="textPrimary">
+                              <strong>Lugar:</strong> {item.city.name} {item.country.name}
+                            </Typography>
+                            <Typography variant="body2" color="textPrimary">
+                              <strong>Nombre:</strong> {item.userReport?.name.first}, {item.userReport?.name.last}
+                            </Typography>
+                          </React.Fragment>
+                        }
+                      />
+                    </ListItem>
+                  </Link>
+                </Grid>
+              ))}
+            </Grid>
+          )}
+        </Paper>
       </Grid>
     </Grid>
   );
